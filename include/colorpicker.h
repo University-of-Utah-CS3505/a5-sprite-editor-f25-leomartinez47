6bdba173f7d5cab251e6783b4ba54f94102e0269
Natalie Bonilla <nataliebonilla2@gmail.com>
2025-11-11 20:19:34 -0700

    fixed committing issues

--- include/colorpicker.h
+++ include/colorpicker.h
@@ -1,22 +1,21 @@
 #ifndef COLORPICKER_H
 #define COLORPICKER_H
 
-#include <QWidget>
-#include <QSlider>
-#include <QLabel>
+#include <QDialog>
+#include <QColor>
 
 class colorpicker : public QWidget
 {
-    Q_OBJECT
-    QSlider *red_slider, *green_slider, *blue_slider;
-
-    QLabel *red_label, *green_label, *blue_label;
 
 public:
-    colorpicker(QWidget *parent = 0);
+
+    explicit colorpicker (QWidget* parent = nullptr);
     ~colorpicker();
+    QColor color() const;
 
+private:
 
+    QColor currentColor;
 };
 
 #endif // COLORPICKER_H
