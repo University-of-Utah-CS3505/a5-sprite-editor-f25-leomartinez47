82811cb1ba9f65e4f90f15c19772f966963e3194
leomartinez47 <leomartinez4723@gmail.com>
2025-11-13 11:35:50 -0700

    Adjusted canvaspane.h and previewpane.h comment formatting.

--- include/canvaspane.h
+++ include/canvaspane.h
@@ -12,45 +12,59 @@
 #include "project.h"
 
 
-/// The CanvasPane is the canvas that the user draws the Sprite on.
+/// The CanvasPane is the canvas widget that the user draws the Sprite on.
 class CanvasPane : public QWidget
 {
     Q_OBJECT
 
 public:
+    ///
     /// @brief Construct a CanvasPane and draws the current frame of the Sprite.
     /// @param project - a pointer to the project model.
     /// @param parent - nullptr because there is no parent.
+    ///
     explicit CanvasPane(Project *project, QWidget *parent = nullptr);
 
 signals:
+    ///
     /// @brief Signal to the project model that the user has clicked on the
     /// canvas at a specific coordinate point.
     /// @param coordinates - The QPointF coordinate point of the click with
     /// respect to the QImage frame itself.
+    ///
     void pixelClicked(QPoint coordinates);
 
 public slots:
+    ///
     /// @brief Recieve a QImage frame from the project model and display it.
     /// @param frame - A QImage to be displayed in the CanvasPane QWidget.
+    ///
     void onFrameChanged(const QImage &frame);
 
 protected:
+    ///
     /// @brief Sets isDrawing to true and emit pointClicked.
     /// @param event - A mouse press QMouseEvent.
+    ///
     void mousePressEvent(QMouseEvent *event) override;
 
+    ///
     /// @brief Sets isDrawing to false.
     /// @param event - A mouse release QMouseEvent.
+    ///
     void mouseReleaseEvent(QMouseEvent *event) override;
 
+    ///
     /// @brief If isDrawing emits pointClicked.
     /// @param event - A mouse move QMouseEvent.
+    ///
     void mouseMoveEvent(QMouseEvent *event) override;
 
+    ///
     /// @brief Draws currentFrame scaled to fit the canvas widget,
     /// and adds a png-style grid background to represent the transparent
     /// parts of the frame.
+    ///
     void paintEvent(QPaintEvent*) override;
 
 private:
@@ -70,8 +84,10 @@ private:
     /// Offset to center the frame vertically.
     int yOffset;
 
+    ///
     /// @brief Maps canvas coordinates to the sprite's pixel coordinates.
     /// @param widgetPos - The cursor position relative to the canvas.
+    ///
     QPoint mapToSprite(const QPoint &widgetPos) const;
 };
 
--- include/previewpane.h
+++ include/previewpane.h
@@ -16,32 +16,49 @@ namespace Ui {
 class PreviewPane;
 }
 
-/// The Preview Pane displays an animation of the frames of the Sprite.
+/// The Preview Pane widget displays an animation of the frames of the Sprite.
 class PreviewPane : public QWidget
 {
     Q_OBJECT
 
 public:
+    ///
+    /// @brief Construct a new PreviewPane.
+    /// @param project - A pointer to the project model.
+    /// @param parent - nullptr because there is no parent.
+    ///
     explicit PreviewPane(Project *project, QWidget *parent = nullptr);
+
+    ///
+    /// @brief Destructs this PreviewPane by deleting the ui form object.
+    ///
     ~PreviewPane();
 
 public slots:
+    ///
     /// @brief Receive when the user starts/stops the animation preview.
+    ///
     void onPlayPauseClicked();
 
+    ///
     /// @brief Paint the current frame in the animation, and updates
     /// the currentFrameIndex appropriately.
+    ///
     void showFrame();
 
 signals:
+    ///
     /// @brief Send the frame rate of the animation to the sprite.
     /// @param - The current frame rate of the animation, in ms per frame.
+    ///
     void setFrameRate(int frameRate);
 
 protected:
-    /// @brief  Paint the current frame in the animation scaled to fit the
+    ///
+    /// @brief Paint the current frame in the animation scaled to fit the
     /// frame widget, and add a png-style grid background to represent
     /// the transparent parts of the frame.
+    ///
     void paintEvent(QPaintEvent*) override;
 
 private:
